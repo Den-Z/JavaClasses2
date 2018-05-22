577cb366f7b4e76ab0f8673a13fcab2a2729eb66
Crackbrain <FunnyDen4uk@gmail.com>
2018-05-22 19:20:03 +0300

    inital project

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package l1;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("HI");
+		System.out.println("HELLO");
+		System.out.println("ZDAROVA");
+		System.out.println("KY KY EPTA");
+		System.out.println("ZDRAVIYA ZELAY");
+	534 456 7765 83145 9651 6754 2824,321 12345,435 
+	}
+
+}
